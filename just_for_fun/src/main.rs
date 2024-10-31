6a317706acd4148647861be13f707cbdf41d9c69
Dmitry Akimov <107173613+OSabuser@users.noreply.github.com>
2024-10-31 10:27:32 +0300

    # FUN 3
    Shitty draw line function!

--- just_for_fun/src/main.rs
+++ just_for_fun/src/main.rs
@@ -19,7 +19,7 @@ impl FrameBuffer {
             buffer: vec![0; width_px * height_px],
         }
     }
-
+    //todo: implement Display trait!
     fn show_buffer(&self) {
         assert!(!self.buffer.is_empty());
 
@@ -42,16 +42,53 @@ impl FrameBuffer {
         assert!(x_pos < self.width);
         assert!(y_pos < self.height);
 
-        let pixel_position: usize = ((y_pos + 1)*self.width ) - (self.width - x_pos);
+        let pixel_position: usize = ((y_pos + 1) * self.width) - (self.width - x_pos);
 
         assert!(pixel_position < self.buffer.len());
 
         self.buffer[pixel_position] = 1;
-
     }
 
-    fn draw_line(&mut self) {
-        todo!("Not implemented yet!");
+    //Bresenham's line algorithm
+    fn draw_line(&mut self, x_coord: (usize, usize), y_coord: (usize, usize)) {
+        // Check whether line coordinates are correct or not
+        assert!((0..self.width).contains(&x_coord.0));
+        assert!((0..self.width).contains(&x_coord.1));
+        assert!((0..self.height).contains(&y_coord.0));
+        assert!((0..self.height).contains(&y_coord.1));
+
+        let delta_x = x_coord.1.abs_diff(x_coord.0);
+        let delta_y = y_coord.1.abs_diff(y_coord.0);
+
+        let mut error = (delta_x as isize) - (delta_y as isize);
+
+        let sign_x = if x_coord.0 < x_coord.1 { 1 } else { -1 };
+
+        let sign_y = if y_coord.0 < y_coord.1 { 1 } else { -1 };
+
+        let mut x: isize = x_coord.0 as isize;
+        let mut y: isize = y_coord.0 as isize;
+
+        'inf_loop: loop {
+            println!("x_pos= {}, y_pos = {}",x as usize, y as usize);
+            self.draw_pixel(x as usize, y as usize);
+
+            if x as usize == x_coord.1 && y as usize == y_coord.1 {
+                break 'inf_loop;
+            }
+
+            let error_2 = error * 2;
+
+            if error_2 > -(delta_y as isize) {
+                error -= delta_y as isize;
+                x += sign_x;
+            }
+
+            if error_2 < delta_x as isize {
+                error += delta_x as isize;
+                y += sign_y;
+            }
+        }
     }
 
     fn draw_rect(&mut self) {
@@ -72,9 +109,7 @@ fn main() {
     );
 
     tft_display.show_buffer();
-    tft_display.draw_pixel(3, 2);
 
+    tft_display.draw_line((0, 15), (1, 5));
     tft_display.show_buffer();
-
-    tft_display.fill_rect();
 }
