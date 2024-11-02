9a4e9d5b624766fa818f0aba717c24bf2ade58d7
Dmitry Akimov <107173613+OSabuser@users.noreply.github.com>
2024-11-02 17:08:17 +0300

    # FUN 5
    small changes

--- just_for_fun/src/main.rs
+++ just_for_fun/src/main.rs
@@ -1,24 +1,26 @@
+//! 
 pub(crate) const MAX_WIDTH_IN_PX: usize = 1024;
 pub(crate) const MAX_HEIGHT_IN_PX: usize = 768;
 
 #[derive(Debug)]
-struct FrameBuffer {
+struct CanvasBuffer {
     width: usize,
     height: usize,
     buffer: Vec<u8>,
 }
 
-impl FrameBuffer {
-    fn new(width_px: usize, height_px: usize) -> FrameBuffer {
+impl CanvasBuffer {
+    fn new(width_px: usize, height_px: usize) -> CanvasBuffer {
         assert!(width_px < MAX_WIDTH_IN_PX + 1);
         assert!(height_px < MAX_HEIGHT_IN_PX + 1);
 
-        FrameBuffer {
+        CanvasBuffer {
             width: width_px,
             height: height_px,
             buffer: vec![0; width_px * height_px],
         }
     }
+    
     //todo: implement Display trait!
     fn show_buffer(&self) {
         assert!(!self.buffer.is_empty());
@@ -34,7 +36,7 @@ impl FrameBuffer {
 
             println!("{row}:    {:?}", buffer_row);
         }
-        println!("");
+        println!();
     }
 
     fn draw_pixel(&mut self, x_pos: usize, y_pos: usize) {
@@ -42,8 +44,8 @@ impl FrameBuffer {
         assert!(x_pos < self.width);
         assert!(y_pos < self.height);
 
-       // let pixel_position: usize = ((y_pos + 1) * self.width) - (self.width - x_pos);
-       let pixel_position: usize = x_pos + y_pos * self.width;
+        // let pixel_position: usize = ((y_pos + 1) * self.width) - (self.width - x_pos);
+        let pixel_position: usize = x_pos + y_pos * self.width;
 
         assert!(pixel_position < self.buffer.len());
 
@@ -92,13 +94,11 @@ impl FrameBuffer {
     }
 
     fn draw_rect(&mut self, x_coord: (usize, usize), y_coord: (usize, usize)) {
-
         self.draw_line((x_coord.0, x_coord.1), (y_coord.0, y_coord.0)); // Top line
         self.draw_line((x_coord.0, x_coord.1), (y_coord.1, y_coord.1)); // Bottom line
 
         self.draw_line((x_coord.0, x_coord.0), (y_coord.0, y_coord.1)); // Left line
         self.draw_line((x_coord.1, x_coord.1), (y_coord.0, y_coord.1)); // Right line
-
     }
 
     fn fill_rect(&mut self, x_coord: (usize, usize), y_coord: (usize, usize), value: u8) {
@@ -108,31 +108,22 @@ impl FrameBuffer {
         assert!((0..self.height).contains(&y_coord.0));
         assert!((0..self.height).contains(&y_coord.1));
 
-
-    
         for row in y_coord.0..=y_coord.1 {
-            let lower_bound = x_coord.0 + row*self.width;
-            let higher_bound = x_coord.1 + row*self.width ;
+            let lower_bound = x_coord.0 + row * self.width;
+            let higher_bound = x_coord.1 + row * self.width;
 
             println!("Row value is {row}");
             self.buffer[lower_bound..=higher_bound].fill(value);
-
-            
         }
-        
-
     }
-
 
     fn fill_buffer(&mut self, value: u8) {
         self.buffer = vec![value; self.width * self.height];
     }
-
-
 }
 
 fn main() {
-    let mut tft_display = FrameBuffer::new(10, 10);
+    let mut tft_display = CanvasBuffer::new(10, 10);
 
     println!(
         "The framebuffer has been created: {}x{} px",
@@ -141,6 +132,4 @@ fn main() {
 
     tft_display.fill_rect((0, tft_display.width - 1), (0, tft_display.height - 1), 1);
     tft_display.show_buffer();
-
-    
 }
