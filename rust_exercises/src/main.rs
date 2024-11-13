db9957bbcb9cbe380326a605cdd41aca91891914
Dmitry Akimov <107173613+OSabuser@users.noreply.github.com>
2024-11-13 18:18:23 +0300

    # 1.2 - the beginning

--- rust_exercises/src/main.rs
+++ rust_exercises/src/main.rs
@@ -1,6 +1,7 @@
 //https://code.mu/ru/rust/tasker/stager/1/1/
 
 //#1
+#[allow(dead_code)]
 fn is_int_less_than_zero(number: i32) -> () {
     match number < 0 {
         true => println!("Отрицательное!"),
@@ -8,10 +9,12 @@ fn is_int_less_than_zero(number: i32) -> () {
     };
 }
 //#2
+#[allow(dead_code)]
 fn show_string_length(input: &str) {
     println!("String length is: {}", input.len());
 }
 //#3
+#[allow(dead_code)]
 fn show_the_last_symbol(input: &str) {
     match input.get(input.len() - 1..) {
         Some(x) => println!("The last char is: {x}"),
@@ -19,13 +22,14 @@ fn show_the_last_symbol(input: &str) {
     };
 }
 //#4
+#[allow(dead_code)]
 fn is_first_chars_the_similar(input_1: &str, input_2: &str) -> bool {
     input_1.starts_with(&input_2[..1])
 }
 
 //#5
+#[allow(dead_code)]
 fn get_the_magic_char(input: &str) -> () {
-
     println!("Input string is: {input}");
     let chars_array = input.chars().rev();
 
@@ -42,36 +46,34 @@ fn get_the_magic_char(input: &str) -> () {
     }
 }
 
-//#5 
+//#5
+#[allow(dead_code)]
 fn convert_int_to_string(value: i32) -> String {
     value.to_string()
 }
 
-
 //#6
+#[allow(dead_code)]
 fn check_if_number_in_range(number: u16, range: (u16, u16)) -> bool {
-    
     number <= range.1 && number >= range.0
-
 }
 
+//https://code.mu/ru/rust/tasker/stager/1/2/
+//#1
+#[allow(dead_code)]
+fn show_the_least_digit_of_number(number: i32) {
+    println!("The least digit is: {}", number % 10);
+}
+//#2
+#[allow(dead_code)]
+fn show_the_most_digit_of_number(number: i32) {
+    match number.to_string().get(..1) {
+        Some(x) => println!("The most digit is: {}", x),
+        None => println!("Error!"),
+    }
+}
 
 fn main() {
-    let num = -123;
-    is_int_less_than_zero(num);
-
-    let txt: &str = "abcde";
-    show_string_length(txt);
-    show_the_last_symbol(txt);
-
-    println!("Result #4 is: {}", is_first_chars_the_similar("abc", "ade"));
-
-    get_the_magic_char("Selamin!ь");
-
-    println!("Converted val is {}", convert_int_to_string(123));
-
-
-    let value = 17;
-    let range = (1, 16);
-    println!("Is {value} in : {}", check_if_number_in_range(value, range));
+    show_the_least_digit_of_number(123);
+    show_the_most_digit_of_number(123);
 }
